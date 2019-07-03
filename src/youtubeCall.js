c1fae5444d0379bcd2bdc76a0bf6f41870aed252
enes <enuxuna@gmail.com>
2019-07-03 09:01:44 +0300

    credential leaks

--- src/youtubeCall.js
+++ src/youtubeCall.js
@@ -1,6 +1,4 @@
 import axios from "axios"
-const API_KEY = "AIzaSyDPYReE9II8CX2-E0QXVbk_hV-q16iZsTQ"
-const OAUTH_KEY = "378257028232-h4b9rapcokl1pgvbqa8ts0aekcf3r7nd.apps.googleusercontent.com"
 
 export default axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3/",
